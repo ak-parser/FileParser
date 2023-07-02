379c96c40aced9748513fbd3a30ba9627c2f5682
Andrii Koval <abarvn@gmail.com>
2023-07-02 17:38:59 +0300

    Add file parser

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+Console.Write("Enter folder path to search: ");
+string rootPath = Console.ReadLine();
+
+Console.Write("Enter files name(for example *.cs): ");
+string[] files = Directory.GetFiles(rootPath, Console.ReadLine(), SearchOption.AllDirectories);
+
+var result = files.Select(path => new { Name = Path.GetFileName(path), Contents = File.ReadAllText(path) })
+                  .Select(info => info.Name + Environment.NewLine + info.Contents);
+
+string singleStr = string.Join(Environment.NewLine, result);
+
+Console.Write("Enter file path for result: ");
+string outputPath = Console.ReadLine();
+
+if (File.Exists(outputPath))
+    File.WriteAllText(outputPath, singleStr, Encoding.UTF8);
+else
+    Console.WriteLine("Error: result file not found");
